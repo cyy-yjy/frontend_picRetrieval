484886ba7cc544cde2d8a3aad237d0878bdf1ac1
a-little-dust <1429692815@qq.com>
2024-05-19 05:15:48 +0800

    1

--- search/src/views/AboutView.vue
+++ search/src/views/AboutView.vue
@@ -1,12 +1,12 @@
 <template>
     <h1>This is an about page</h1>
     <el-button> 刷新</el-button>
-    <!-- 分页 -->
-      <el-pagination background layout="prev, pager, next" :total="pageNum" />
+    
+    
 </template>
 <script>
 import { ref, onMounted } from 'vue'
-const pageNum = ref(1)
+
 </script>
 
 <style scoped></style>
